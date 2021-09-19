f39c6568bef12dd7a38e49d3e5f4cd450b11470d
Svebor1 <svebors2@gmail.com>
2021-09-19 14:47:52 +0200

    dodan dropdown za kategorije#2

--- app/src/main/java/com/example/coinsmart/UnosTroskovaActivity.java
+++ app/src/main/java/com/example/coinsmart/UnosTroskovaActivity.java
@@ -3,12 +3,20 @@ package com.example.coinsmart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class UnosTroskovaActivity extends AppCompatActivity {
-
+    private Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unos_troskova);
+        spinner = findViewById(R.id.spinner);
+        String kategorije[] = {"prehrana","kućanstvo","promet"};
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, kategorije);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
     }
+
 }
