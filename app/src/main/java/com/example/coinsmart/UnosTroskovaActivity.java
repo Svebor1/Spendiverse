6287555320727c27d32807660626d99da2c8fac9
Svebor1 <svebors2@gmail.com>
2021-09-26 12:44:28 +0200

    dodan datum u textview za unos troškova #2

--- app/src/main/java/com/example/coinsmart/UnosTroskovaActivity.java
+++ app/src/main/java/com/example/coinsmart/UnosTroskovaActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class UnosTroskovaActivity extends AppCompatActivity {
     private Spinner spinner;
@@ -19,10 +22,22 @@ public class UnosTroskovaActivity extends AppCompatActivity {
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, kategorije);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(arrayAdapter);
+        prikaziDatum();
+
     }
     public void showDatePickerDialog(View v) {
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getSupportFragmentManager(), "datePicker");
-    } //da
+    }
+    private void prikaziDatum() {
+
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH)+1;
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        TextView datumTroska;
+        datumTroska = findViewById(R.id.datum_troska);
+        datumTroska.setText(Integer.toString(day) + "." + Integer.toString(month) + "." + Integer.toString(year)); //
+    }
 
 }
