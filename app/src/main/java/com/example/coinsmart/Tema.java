f42ad06213f37703f0ff1a72fc998756038a4813
AtramLj <marta.ljubic08@gmail.com>
2021-12-28 18:53:58 +0100

    moze se otvoriti pitanje

--- app/src/main/java/com/example/coinsmart/Tema.java
+++ app/src/main/java/com/example/coinsmart/Tema.java
@@ -2,7 +2,11 @@ package com.example.coinsmart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Tema extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class Tema extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tema);
+        Button pocetakKviza;
+        pocetakKviza=findViewById(R.id.kviz_start);
+        pocetakKviza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                prikazPitanja();
+            }
+        });
+    }
+    private void prikazPitanja() {
+        Intent intent = new Intent(this, Pitanje.class);
+        startActivity(intent);
     }
 }
