1f79f0547196631a22444290b27cef203746e0d9
AtramLj <marta.ljubic08@gmail.com>
2021-12-28 19:45:46 +0100

    dodane probne slike za slideshow

--- app/src/main/java/com/example/coinsmart/Tema.java
+++ app/src/main/java/com/example/coinsmart/Tema.java
@@ -8,6 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel;
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Tema extends AppCompatActivity {
 
     @Override
@@ -22,6 +30,57 @@ public class Tema extends AppCompatActivity {
                 prikazPitanja();
             }
         });
+        ImageCarousel carousel = findViewById(R.id.carousel);
+
+// Register lifecycle. For activity this will be lifecycle/getLifecycle() and for fragments it will be viewLifecycleOwner/getViewLifecycleOwner().
+        carousel.registerLifecycle(getLifecycle());
+
+        List<CarouselItem> list = new ArrayList<>();
+
+// Image URL with caption
+        list.add(
+                new CarouselItem(
+                        "https://images.unsplash.com/photo-1532581291347-9c39cf10a73c?w=1080",
+                        "Photo by Aaron Wu on Unsplash"
+                )
+        );
+
+// Just image URL
+        list.add(
+                new CarouselItem(
+                        "https://images.unsplash.com/photo-1534447677768-be436bb09401?w=1080"
+                )
+        );
+
+// Image URL with header
+        Map<String, String> headers = new HashMap<>();
+        headers.put("header_key", "header_value");
+
+        list.add(
+                new CarouselItem(
+                        "https://images.unsplash.com/photo-1534447677768-be436bb09401?w=1080",
+                        headers
+                )
+        );
+
+// Image drawable with caption
+        list.add(
+                new CarouselItem(
+                        R.drawable.kasica_ljudi_novac,
+                        "Photo by Kimiya Oveisi on Unsplash"
+                )
+        );
+
+// Just image drawable
+        list.add(
+                new CarouselItem(
+                        R.drawable.rich_bankar
+                )
+        );
+
+// ...
+
+        carousel.setData(list);
     }
     private void prikazPitanja() {
         Intent intent = new Intent(this, Pitanje.class);
