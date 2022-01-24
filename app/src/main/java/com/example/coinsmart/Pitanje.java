80bc371ec278f9576e6a0fdfbe8079ad6c3a8bb1
AtramLj <marta.ljubic08@gmail.com>
2022-01-24 20:50:40 +0100

    promjenjena ikona za alert kod rezultata

--- app/src/main/java/com/example/coinsmart/Pitanje.java
+++ app/src/main/java/com/example/coinsmart/Pitanje.java
@@ -160,7 +160,7 @@ public class Pitanje extends AppCompatActivity {
 
                         // A null listener allows the button to dismiss the dialog and take no further action.
                         .setNegativeButton(android.R.string.no, null)
-                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setIcon(R.drawable.slika_rezultat)
                         .create();
         alertDialog.show();
     }
