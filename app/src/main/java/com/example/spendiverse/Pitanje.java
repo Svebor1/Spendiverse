f25752b51daed369d7dcae5efe98b5834e12ecc9
AtramLj <marta.ljubic08@gmail.com>
2022-02-12 21:28:44 +0100

    konfeti rade za 80% ili vise

--- app/src/main/java/com/example/spendiverse/Pitanje.java
+++ app/src/main/java/com/example/spendiverse/Pitanje.java
@@ -179,18 +179,20 @@ public class Pitanje extends AppCompatActivity {
         }
     }
     private void prikaziRezultate() {
-        final KonfettiView konfettiView = findViewById(R.id.viewKonfetti);
-        konfettiView.build()
-                .addColors(Color.rgb(247, 184, 1),Color.rgb(118, 120, 238))
-                .setDirection(0.0, 359.0)
-                .setSpeed(1f, 5f)
-                .setFadeOutEnabled(true)
-                .setTimeToLive(2000L)
-                .addShapes(Shape.Square.INSTANCE, Shape.Circle.INSTANCE)
-                //.addSizes(new Size(12, 5f))
-                .setPosition(-50f, konfettiView.getWidth() + 50f, -50f, -50f)
-                .streamFor(300, 5000L);
         Integer bodovi = izracunajRezultat();
+        if ((bodovi.floatValue()/kolicinaPitanja.floatValue()*100)>=80) {
+            final KonfettiView konfettiView = findViewById(R.id.viewKonfetti);
+            konfettiView.build()
+                    .addColors(Color.rgb(247, 184, 1), Color.rgb(118, 120, 238))
+                    .setDirection(0.0, 359.0)
+                    .setSpeed(1f, 5f)
+                    .setFadeOutEnabled(true)
+                    .setTimeToLive(2000L)
+                    .addShapes(Shape.Square.INSTANCE, Shape.Circle.INSTANCE)
+                    //.addSizes(new Size(12, 5f))
+                    .setPosition(-50f, konfettiView.getWidth() + 50f, -50f, -50f)
+                    .streamFor(300, 5000L);
+        }
         AlertDialog alertDialog =
                 new AlertDialog.Builder(this)
                         .setTitle("Rezultat")
