1c6b5d925e62c256ee9ab53474d6474eb0dda8b8
AtramLj <marta.ljubic08@gmail.com>
2022-02-13 10:42:25 +0100

    napisani komentari i stavljen pocetni datum za financijski plan

--- app/src/main/java/com/example/spendiverse/FinancijskiPlanActivity.java
+++ app/src/main/java/com/example/spendiverse/FinancijskiPlanActivity.java
@@ -23,81 +23,106 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
 public class FinancijskiPlanActivity extends AppCompatActivity {
+    //varijable za sve Spinnere u layoutu
     private Spinner spinnerMjeseci;
     private Spinner spinnerGodine;
+    //za debugiranje
     private final String TAG = "FinancijskiPlanActivity";
+    //za dobivanje trenutnog datuma
+    private final Calendar myCalendar = Calendar.getInstance();
+    //varijable za sve TextViewove u layoutu
     private TextView dzeparac;
     private TextView poslovi;
     private TextView pokloni;
     private TextView ostalo;
     private TextView ustedzevina;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_financijski_plan);
+        //pronalazi TextViewove prema id-u
         dzeparac = findViewById(R.id.dzeparac_upis);
         poslovi = findViewById(R.id.poslovi_upis);
         pokloni = findViewById(R.id.pokloni_upis);
         ostalo = findViewById(R.id.ostalo_upis);
         ustedzevina = findViewById(R.id.ustedzevina_upis);
+        //pronalazi Button po id-u
         Button azurirajPlan = findViewById(R.id.azuriraj_plan);
+
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dodajNoviPlan();
             }
         };
+        //postavlja novi plan na pritisak Buttona
         azurirajPlan.setOnClickListener(listener);
 
+        //Pronalazi treutni datum
+        Integer trenutnaGodina = myCalendar.get(Calendar.YEAR);
+        Integer trenutniMjesec = myCalendar.get(Calendar.MONTH)+1;
+
+        //postavljanje lista s izborima za mjesece i godine
         String mjeseci[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
-
         spinnerMjeseci = findViewById(R.id.spinner_mjeseci);
-        ArrayAdapter arrayAdapterMjeseci = new ArrayAdapter(this, android.R.layout.simple_spinner_item, mjeseci);
-        arrayAdapterMjeseci.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerMjeseci.setAdapter(arrayAdapterMjeseci);
-        spinnerMjeseci.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        postaviSpinner(mjeseci, trenutniMjesec.toString(), spinnerMjeseci);
 
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                String mjesec = spinnerMjeseci.getSelectedItem().toString();
-                String godine = spinnerGodine.getSelectedItem().toString();
-                nadiTroskove(mjesec,godine);
-                prikazPlana(mjesec,godine);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-            }
-
-        });
         String godine[] = {"2022","2021","2020","2019","2018"};
-
         spinnerGodine = findViewById(R.id.spinner_godine);
-        ArrayAdapter arrayAdapterGodine = new ArrayAdapter(this, android.R.layout.simple_spinner_item, godine);
-        arrayAdapterGodine.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerGodine.setAdapter(arrayAdapterGodine);
-        spinnerGodine.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                String godine = spinnerGodine.getSelectedItem().toString();
-                String mjesec = spinnerMjeseci.getSelectedItem().toString();
-                nadiTroskove(mjesec,godine);
-                prikazPlana(mjesec,godine);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-            }
-
-        });
+        postaviSpinner(godine, trenutnaGodina.toString(), spinnerGodine);
     }
 
+    /**
+     *Postavlja izbore za Spinner i stavlja trenutni zbor kao početni.
+     *
+     * @param vrijednosti lista koja sadržava elemente koji se mogu izabrati
+     * @param trenutniIzbor element iz liste vrijednosti koji treba izabrati u Spinneru
+     * @param spinner Spinner koji učitava izbore
+     */
+    private void postaviSpinner(String[] vrijednosti,String trenutniIzbor, Spinner spinner){
+        //Stvara Adapter za listu vrijednosti
+        ArrayAdapter vrijednostiAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, vrijednosti);
+        //Određuje izgled ponuđenih izbora u Spinneru
+        vrijednostiAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Spinner se povezuje s Adapterom
+        spinner.setAdapter(vrijednostiAdapter);
+        //traži izbor koji želimo postaviti unutar ponuđenih izbora u Spinneru
+        int spinnerPosition = vrijednostiAdapter.getPosition(trenutniIzbor.toString());
+        spinner.setSelection(spinnerPosition);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                //Prikazuje plan za izabranu godinu i mjesec
+                String godine = spinnerGodine.getSelectedItem().toString();
+                String mjesec = spinnerMjeseci.getSelectedItem().toString();
+                nadiTroskove(mjesec,godine);
+                prikazPlana(mjesec,godine);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+
+        });
+
+    }
+
+    /**
+     * Funkcija koja u bazi traži ukupan iznos troškova u određenoj godini i mjesecu za trenutno
+     * prijavljenog korisnika.
+     *
+     * @param mjesec izabrani mjesec
+     * @param godine izabrana godina
+     */
     private void  nadiTroskove(String mjesec,String godine) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
@@ -126,6 +151,11 @@ public class FinancijskiPlanActivity extends AppCompatActivity {
                 });
     }
 
+
+    /**
+     * Uzima sadržaj upisan u TextViewove i dodaje ga u novi financijski plan za
+     * izabranu godinu i mjesec.
+     */
     private void dodajNoviPlan() {
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         Map<String, Object> data = new HashMap<>();
@@ -152,6 +182,12 @@ public class FinancijskiPlanActivity extends AppCompatActivity {
                 });
     }
 
+    /**
+     * Uzima upisane podatke iz baze i prikazuje napravljen plan za izabrani mjesec i godinu trenutno prijavljenog korisnika.
+     *
+     * @param mjesec izabrani mjesec
+     * @param godine izabrana godina
+     */
     private void prikazPlana(String mjesec,String godine) {
         ocistiTekst();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -186,6 +222,10 @@ public class FinancijskiPlanActivity extends AppCompatActivity {
                 });
 
     }
+
+    /**
+     * Čisti TextVieweve prije prikazivanja plana.
+     */
     private void ocistiTekst(){
         dzeparac.setText("");
         poslovi.setText("");
