b70022df1e48fbc0ea263883861dc8784ba74ab9
Svebor1 <svebors2@gmail.com>
2022-03-05 21:47:47 +0100

    dodana mogućnost brisanja korisničkog profila, ne dovršeno

--- app/src/main/java/com/example/spendiverse/MojProfil.java
+++ app/src/main/java/com/example/spendiverse/MojProfil.java
@@ -18,6 +18,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -31,11 +32,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class MojProfil extends AppCompatActivity {
@@ -50,6 +47,7 @@ public class MojProfil extends AppCompatActivity {
     List<Rezultat> rezultati;
     Switch prikazNaLjestvici;
     ImageButton editNadimka;
+    Button brisanjeRacuna;
     String TAG = "MojProfil";
     private String nadimak;
 
@@ -70,10 +68,34 @@ public class MojProfil extends AppCompatActivity {
         nadimakKorisnika = findViewById(R.id.nadimak_korisnika);
         editNadimka = findViewById(R.id.edit_nadimka);
         prikazNaLjestvici = findViewById(R.id.switch1);
-
+        brisanjeRacuna = findViewById(R.id.brisanje_racuna);
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         db = FirebaseFirestore.getInstance();
 
+        AlertDialog alertDialogBrisanje =
+                new AlertDialog.Builder(this)
+                        .setTitle("Brisanje profila")
+                        .setMessage("Jeste li sigurni da želite izbrisati profil?")
+                        .setPositiveButton("Da", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                izbrisiRacun();
+                            }
+                        })
+                        .setNegativeButton("Ne", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .setIcon(R.drawable.ic_baseline_help_24)
+                        .create();
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialogBrisanje.show();
+            }
+        };
+        brisanjeRacuna.setOnClickListener(listener);
         if (firebaseUser != null) {
             // Name, email address, and profile photo Url
             String email = firebaseUser.getEmail();
@@ -128,13 +150,13 @@ public class MojProfil extends AppCompatActivity {
             }
         });
 
-        View.OnClickListener listener = new View.OnClickListener() {
+        View.OnClickListener listener3 = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 prikaziLjestvicu();
             }
         };
-        prikazLjestvice.setOnClickListener(listener);
+        prikazLjestvice.setOnClickListener(listener3);
         procitajRezultate();
 
     }
@@ -225,4 +247,52 @@ public class MojProfil extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    private void izbrisiRacun() {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("korisnici").document(user.getUid())
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully deleted!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error deleting document", e);
+                    }
+                });
+        db.collection("ljestvica").document(user.getUid())
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully deleted!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error deleting document", e);
+                    }
+                });
+        if (user != null) {
+            user.delete()
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Log.d("TAG", "User account deleted.");
+                                startActivity(new Intent(MojProfil.this, MainActivity.class));
+                                Toast.makeText(MojProfil.this, "Vaš profil je izbrisan", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+
+        }
+
+    }
+
 }
