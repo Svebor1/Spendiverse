2c797d9f6d22bd95ea009bd5fedcea81318b19ec
Svebor1 <svebors2@gmail.com>
2022-03-20 15:11:43 +0100

    dodan prikaz kategorija u activityu s unosom novih kategorija #13

--- app/src/main/java/com/example/spendiverse/DodavanjeKategorijeTroska.java
+++ app/src/main/java/com/example/spendiverse/DodavanjeKategorijeTroska.java
@@ -1,36 +1,51 @@
 package com.example.spendiverse;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class DodavanjeKategorijeTroska extends AppCompatActivity {
-
+    String TAG = "DodavanjeKategorijeTroska";
     EditText nazivKategorijeUnos;
     Button dodajKategorijuTroskaButton;
     FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-
+    String zadaneKategorije[] = {"prehrana","kućanstvo","promet"};
+    ArrayList<String> kategorije = new ArrayList<>(Arrays.asList(zadaneKategorije));
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodavanje_kategorije_troska);
         nazivKategorijeUnos = findViewById(R.id.naziv_kategorije);
         dodajKategorijuTroskaButton = findViewById(R.id.dodaj_kategoriju_troska);
+
+        prikaziKategorije();
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dodajKategoriju();
+                prikaziKategorije();
             }
         };
         dodajKategorijuTroskaButton.setOnClickListener(listener);
@@ -42,5 +57,33 @@ public class DodavanjeKategorijeTroska extends AppCompatActivity {
         db.collection("korisnici").document(firebaseUser.getUid())
                 .collection("kategorije").add(data);
 
+
+    }
+    private void prikaziKategorije() {
+        kategorije = new ArrayList<>(Arrays.asList(zadaneKategorije));
+        Context context = this;
+        db.collection("korisnici").document(firebaseUser.getUid()).collection("kategorije")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                String nazivKategorije = document.getData().get("naziv").toString();
+
+                                kategorije.add(nazivKategorije);
+                            }
+                            // prikaziTroskove(poredajPo.getSelectedItem().toString(), filterKategorija.getSelectedItem().toString(), filterzaRazdoblja.getSelectedItem().toString(), filterzaValute.getSelectedItem().toString());
+                            ArrayAdapter<String> itemsAdapter =
+                                    new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, kategorije);
+                            ListView listView = (ListView) findViewById(R.id.kategorije_troska);
+                            listView.setAdapter(itemsAdapter);
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+
+                    }
+                });
     }
 }
