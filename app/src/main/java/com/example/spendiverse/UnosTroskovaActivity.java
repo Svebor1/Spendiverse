1d7c3f400851efd933109a97bb1f64259924958c
AtramLj <marta.ljubic08@gmail.com>
2022-03-20 15:21:26 +0100

    pocetak dodavanja slike racuna #17

--- app/src/main/java/com/example/spendiverse/UnosTroskovaActivity.java
+++ app/src/main/java/com/example/spendiverse/UnosTroskovaActivity.java
@@ -5,15 +5,22 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -21,7 +28,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
@@ -33,11 +44,14 @@ public class UnosTroskovaActivity extends AppCompatActivity {
     private final Calendar myCalendar = Calendar.getInstance();
     private TextView nazivTroska;
     private Button dodatiTrosak;
+    private ImageButton dodatiRacun;
     private Integer dan;
     private Integer mjesec;
     private Integer godina;
+    private ImageView slikaRacuna;
     private TextView cijenaTroska;
     private TextView datumTroska;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,17 +62,19 @@ public class UnosTroskovaActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         spinner = findViewById(R.id.spinner);
         spinnerValuta = findViewById(R.id.spinner_valuta);
+        slikaRacuna = findViewById(R.id.slika_racuna);
         datumTroska = findViewById(R.id.datum_troska);
         dodatiTrosak = findViewById(R.id.dodati_trosak);
+        dodatiRacun = findViewById(R.id.dodati_racun);
         nazivTroska = findViewById(R.id.naziv_troska);
         cijenaTroska = findViewById(R.id.cijena_troska);
-        String kategorije[] = {"prehrana","kućanstvo","promet"};
+        String kategorije[] = {"prehrana", "kućanstvo", "promet"};
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.spinner_item, kategorije);
         arrayAdapter.setDropDownViewResource(R.layout.spinner_item);
         spinner.setAdapter(arrayAdapter); //postavljanje niz mogućih kategorija u izbornik za kategorije
 
-        String valute[] = {"HRK","USD","EUR", "GBP"};
-        ArrayAdapter arrayAdapterValute = new ArrayAdapter(this, R.layout.spinner_item , valute);
+        String valute[] = {"HRK", "USD", "EUR", "GBP"};
+        ArrayAdapter arrayAdapterValute = new ArrayAdapter(this, R.layout.spinner_item, valute);
         arrayAdapter.setDropDownViewResource(R.layout.spinner_item);
         spinnerValuta.setAdapter(arrayAdapterValute);
 
@@ -75,8 +91,16 @@ public class UnosTroskovaActivity extends AppCompatActivity {
                 }
             };
             dodatiTrosak.setOnClickListener(listener);
-        }
-        else {
+            dodatiRacun.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, 1);
+                }
+            });
+
+
+        } else {
             //uzimaju se podaci starog troška i prikazuju se na ekranu
             String nazivTroskaText = bundle.getString("naziv");
             nazivTroska.setText(nazivTroskaText);
@@ -92,7 +116,7 @@ public class UnosTroskovaActivity extends AppCompatActivity {
 
             String firebaseIdTroska = bundle.getString("firebaseId");
             Integer datumDan = bundle.getInt("datumDan");
-            Integer datumMjesec = bundle.getInt("datumMjesec")-1;
+            Integer datumMjesec = bundle.getInt("datumMjesec") - 1;
             Integer datumGodina = bundle.getInt("datumGodina");
             postaviDatum(datumDan, datumMjesec, datumGodina);
             prikaziDatum();
@@ -109,6 +133,7 @@ public class UnosTroskovaActivity extends AppCompatActivity {
 
         }
     }
+
     public void showDatePickerDialog(View v) {
         DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
 
@@ -129,7 +154,8 @@ public class UnosTroskovaActivity extends AppCompatActivity {
 
     /**
      * ova metoda postavlja datum troška i prima tri parametra
-     * @param dan dan troška
+     *
+     * @param dan    dan troška
      * @param mjesec mjesec troška
      * @param godina godina troška
      */
@@ -144,7 +170,7 @@ public class UnosTroskovaActivity extends AppCompatActivity {
      */
     private void prikaziDatum() {
         godina = myCalendar.get(Calendar.YEAR);
-        mjesec = myCalendar.get(Calendar.MONTH)+1;
+        mjesec = myCalendar.get(Calendar.MONTH) + 1;
         dan = myCalendar.get(Calendar.DAY_OF_MONTH);
         datumTroska.setText(Integer.toString(dan) + "." + Integer.toString(mjesec) + "." + Integer.toString(godina));
     }
@@ -214,9 +240,10 @@ public class UnosTroskovaActivity extends AppCompatActivity {
 
     /**
      * ova metoda provjerava je su li sva potrebna polja ispunjena
+     *
      * @return
      */
-    private boolean provjeriUnos(){
+    private boolean provjeriUnos() {
         boolean rezultatBooleana = true;
 
         if (cijenaTroska.getText().toString().equals("")) {
@@ -230,6 +257,7 @@ public class UnosTroskovaActivity extends AppCompatActivity {
 
         return rezultatBooleana;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -238,5 +266,43 @@ public class UnosTroskovaActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap photo;
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            photo = (Bitmap) data.getExtras().get("data");
+            slikaRacuna.setImageBitmap(photo);
+            /*int dimensionInPixel = 50;
+            int dimensionInDp = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dimensionInPixel, getResources().getDisplayMetrics());
+            slikaRacuna.getLayoutParams().height = dimensionInDp;
+            slikaRacuna.requestLayout();*/
+        }
+
+    }
+
+    public void submit(Bitmap photo) {
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        photo.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        StorageReference racunRef = storageRef.child("racun.jpg");
+        byte[] b = stream.toByteArray();
+
+        UploadTask uploadTask = racunRef.putBytes(b);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+            }
+        });
     }
 }
