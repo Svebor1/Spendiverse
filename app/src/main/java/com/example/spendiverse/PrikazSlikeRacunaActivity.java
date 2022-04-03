2586947370159102f11836c6439bae8ba9827996
AtramLj <marta.ljubic08@gmail.com>
2022-04-03 11:59:38 +0200

    dodavanje strelice za natrag u prikazu slike racuna i info

--- app/src/main/java/com/example/spendiverse/PrikazSlikeRacunaActivity.java
+++ app/src/main/java/com/example/spendiverse/PrikazSlikeRacunaActivity.java
@@ -1,9 +1,11 @@
 package com.example.spendiverse;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 public class PrikazSlikeRacunaActivity extends AppCompatActivity {
@@ -12,9 +14,22 @@ public class PrikazSlikeRacunaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prikaz_slike_racuna);
+        //postavljanje strelice natrag
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.arrow_back);
+        actionBar.setDisplayHomeAsUpEnabled(true);
         Bundle bundle = getIntent().getExtras();
         Bitmap slikaRacuna = bundle.getParcelable("slika");
         ImageView slikaRacunaPrikaz = findViewById(R.id.slika_racuna_fullscreen);
         slikaRacunaPrikaz.setImageBitmap(slikaRacuna);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
