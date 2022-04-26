91e1208dcd1ad22fe47d67946bdd8835043146fd
AtramLj <marta.ljubic08@gmail.com>
2022-04-26 23:56:22 +0200

    dodan test #54

---
+++ app/src/test/java/com/example/spendiverse/PomocneFunkcijeTest.java
@@ -0,0 +1,24 @@
+package com.example.spendiverse;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PomocneFunkcijeTest {
+    @Test
+    public void provjeraDuljineUnosa_radi(){
+        String errorZaZnamenke = "Trošak ne može biti veći od 99999999. ";
+        String errorZaDecimale = "Trošak ne može imati više od 2 decimale.";
+        String greska = PomocneFunkcije.provjeraDuljineUnosa("999999999999.4342234");
+        assertEquals(errorZaZnamenke+errorZaDecimale, greska);
+
+        String greska2 = PomocneFunkcije.provjeraDuljineUnosa("999999999999.43");
+        assertEquals(errorZaZnamenke, greska2);
+
+        String greska3 = PomocneFunkcije.provjeraDuljineUnosa("999.4342234246565");
+        assertEquals(errorZaDecimale, greska3);
+
+        String greska4 = PomocneFunkcije.provjeraDuljineUnosa("999.34");
+        assertEquals("", greska4);
+    }
+}
